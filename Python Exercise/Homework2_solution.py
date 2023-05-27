71126854d9817db80cdba11c2100924afe0929ae
alakhpandya <alakhpandya@gmail.com>
2023-05-27 19:05:55 +0530

    may27

---
+++ Python Exercise/Homework2_solution.py
@@ -0,0 +1,25 @@
+# 1
+"""
+def func(x):
+    return 3 * (x ** 2) + 2
+
+x = int(input())
+h = int(input())
+fx = func(x)
+fxh = func(x + h)
+ans = (fxh - fx)/h
+print(ans)
+"""
+# 2
+"""
+a = int(input())
+b = int(input())
+c = int(input())
+print(c > a and c > b)
+"""
+
+import math
+
+a = float(input())
+print("floor:", math.floor(a/200))  # -4
+print("ceil:", math.ceil(a/200))    # -3
